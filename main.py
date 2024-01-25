881ba18c8decb2b74c6feaafae930ff058d42373
AhnJinSung <jinsung9737@gmail.com>
2024-01-25 14:42:57 +0900

    네번째 커밋

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def add_func(n1,n2):
 def sub_func(n1,n2):
     return n1-n2
 
-def sub_func(n1,n2):
+def mul_func(n1,n2):
     return n1*n2
 
-def sub_func(n1,n2):
+def div_func(n1,n2):
     return n1/n2
 
 
@@ -26,4 +26,10 @@ print(num1, '+', num2, '=' , res)
 
 res=sub_func(num1, num2)
 print(num1, '-', num2, '=' , res)
+
+res=mul_func(num1, num2)
+print(num1, '*', num2, '=' , res)
+
+res=div_func(num1, num2)
+print(num1, '/', num2, '=' , res)
 
